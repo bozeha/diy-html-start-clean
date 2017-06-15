bc7330217ca815c22c56eee68c6832728da7b934
BOZE BOZE <BOZE BOZE>
2017-06-15 10:43:26 +0300

    remove popup background and add settimout to popup x button

--- scripts/pop-up-ad.js
+++ scripts/pop-up-ad.js
@@ -7,20 +7,27 @@ $("document").ready(function(){
 setTimeout(function(){
 var php_file = "<?php include 'ads/pop-up.php'; ?>";
 $('body').append("<div id='pop_up_ad'><div id='close_pop'></div><div id='pop_cont'></div></div>")
+$('#pop_cont').load('ads/pop-up.php');
+
+
 $("#close_pop").click(function() {
 switchPopUp(false);
 });
 
+    switchPopUp(true)
+setTimeout(function(){
 
-$('#pop_cont').load('ads/pop-up.php');
-  switchPopUp(true);
+  $('#close_pop').css('display','block')
+}, 2000);
+
+
 }, 5000);
-
 })
 
 
 function switchPopUp(bool)
 {
     (bool == true)?$('#pop_up_ad').css('display','block'):$('#pop_up_ad').css('display','none');
-
+ 
 }
+
