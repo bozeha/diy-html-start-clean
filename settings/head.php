74bde4623faf95b3b158d3fc7b793de1c7159c74
boze - isracard <bozeha@gmail.com>
2017-10-26 12:15:49 +0300

    add sass and fixe top main style

--- settings/head.php
+++ settings/head.php
@@ -3,17 +3,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="style/style_control.css">
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
+<!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
+<!-- bootstrap 4 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 <!-- Optional theme -->
 <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
  
-<script src="assets/js/bootstrap.min.js"></script>
- <link href="style.css" rel="stylesheet">
+<!-- <script src="assets/js/bootstrap.min.js"></script> -->
+ <link href="style/style.css" rel="stylesheet">
+ <link href="style/new-styles.css" rel="stylesheet">
  <script src='scripts/main_scripts.js'></script>
  <script src='scripts/security.js'></script>
  <script src='scripts/meta.js'></script>
  <link rel="icon" type="image/png" href="images/web/favicon.ico">
-<link href="https://fonts.googleapis.com/css?family=Suez+One" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Suez+One" rel="stylesheet">
